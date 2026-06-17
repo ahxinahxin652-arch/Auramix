0ebf8a5a020dec7bb6d9279ba153075d119819f7
黎浅 <1759903257@qq.com>
2026-06-17 18:56:32 +0800

    项目初始化

---
+++ src/main/java/com/son/auramix/AuramixApplication.java
@@ -0,0 +1,13 @@
+package com.son.auramix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuramixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuramixApplication.class, args);
+    }
+
+}
