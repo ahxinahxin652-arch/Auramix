9df5959c423fcba33fdc38702ed18972358ea3e9
Ahxin <ahxinahxin652@gmail.com>
2026-06-29 15:54:35 +0800

    fix 修复数据流转问题： 远程全量拉取后缓存关联数据至本地

--- src/main/java/com/son/auramix/service/user/UserArtistService.java
+++ src/main/java/com/son/auramix/service/user/UserArtistService.java
@@ -2,7 +2,11 @@ package com.son.auramix.service.user;
 
 import com.son.auramix.domain.vo.user.UserArtistDetailVO;
 
+import com.son.auramix.common.result.PageResult;
+import com.son.auramix.domain.vo.user.UserArtistSyncVO;
+
 public interface UserArtistService {
+    PageResult<UserArtistSyncVO> listFollowed(Integer pageNum, Integer pageSize);
     UserArtistDetailVO getArtistDetail(Long artistId);
     
     void followArtist(Long artistId);
