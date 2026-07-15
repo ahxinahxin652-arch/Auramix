c1f14435b004193df20f83e8d9e33ac667ed7f58
Ahxin <ahxinahxin652@gmail.com>
2026-07-15 11:00:14 +0800

    structure

--- run-mvn.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "JAVA_HOME=D:\jdk-21"
-set "PATH=D:\jdk-21\bin;D:\BaseApp\apache-maven-3.9.8\bin;%PATH%"
-cd /d "F:\Code\Auramix\Auramix"
-"D:\BaseApp\apache-maven-3.9.8\bin\mvn.cmd" %*
